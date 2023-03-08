17c4fb4d355b227a22930f413d4857f844a8ac8a
shashank <shashank.godala@gmail.com>
2023-03-08 00:19:42 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const element = (
+  <div className="bg-container">
+    <h1 className="heading">Super Over League</h1>
+    <div className="images">
+      <img
+        className="img1"
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+      />
+      <img
+        className="img1"
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
